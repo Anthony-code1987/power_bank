42b7e85e104235245d967b3ef37d9e9a9ea17a47
Anthonny Dupuis Doudot <adupuis.anthony@gmail.com>
2023-12-06 12:07:59 +0100

    done cards by profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :profile
-
-  # def profile
-  #   @powerbank = Powerbank.find(params[:id])
-  # end
+  def profile
+    @powerbank = current_user.powerbanks
+  end
 end
